e5bbfadbb50ab223f38a7ede444b99482fcf012c
rusconn <rusconn3@gmail.com>
2024-03-31 21:08:35 +0900

    feat: avoid fading out the main screen when searching

--- app/root.tsx
+++ app/root.tsx
@@ -109,7 +109,9 @@ export default function App() {
           </nav>
         </div>
         <div
-          className={navigation.state === "loading" ? "loading" : ""}
+          className={
+            navigation.state === "loading" && !searching ? "loading" : ""
+          }
           id="detail"
         >
           <Outlet />
